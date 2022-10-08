875e75cbf7934d10b4aa6fba1832ae88f1a5fe75
vuquangmt <89431382+vuquangmt@users.noreply.github.com>
2022-10-08 19:21:42 +0700

    KIEMTRA

---
+++ src/ExamPractice/Bank.java
@@ -0,0 +1,31 @@
+package ExamPractice;
+
+public class Bank {
+    private double balance;
+    private double rate;
+
+    public Bank(double balance, double rate) {
+        this.balance = balance;
+        this.rate = rate;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+    public double calculateInterest(){
+        double interest = balance * (rate / 1200);
+        return interest;
+    }
+}
---
+++ src/ExamPractice/Main.java
@@ -0,0 +1,8 @@
+package ExamPractice;
+
+public class Main {
+    public static void main(String[] args){
+        Bank e = new Bank(1000,10);
+        System.out.println(e.calculateInterest());
+    }
+}
